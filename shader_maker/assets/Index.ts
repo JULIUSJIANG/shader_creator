d7f70ae7115701417207f31c1f6d5deb1104d972
DESKTOP-UH0M1SL\86134 <2662774600@qq.com>
2021-08-19 01:53:04 +0800

    让程序能够展示出一个变灰的角色图，添加 cocoscreator 默认 effect

---
+++ shader_maker/assets/Index.ts
@@ -0,0 +1,34 @@
+
+import { _decorator, Component, Node, macro, view, CCLoader, Sprite } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Index')
+export class Index extends Component {
+    // [1]
+    // dummy = '';
+
+    // [2]
+    // @property
+    // serializableDummy = 0;
+
+    @property(Sprite)
+    bg: Sprite = null;
+
+    start () { 
+    }
+
+    // update (deltaTime: number) {
+    //     // [4]
+    // }
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.0/manual/en/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.0/manual/en/scripting/ccclass.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.0/manual/en/scripting/life-cycle-callbacks.html
+ */

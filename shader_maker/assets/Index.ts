c6bdafe06c61710933169d77884615c5bb271d97
DESKTOP-UH0M1SL\86134 <2662774600@qq.com>
2021-08-26 00:29:49 +0800

    成功实验出 cocos 后处理的可行性

--- shader_maker/assets/Index.ts
+++ shader_maker/assets/Index.ts
@@ -1,11 +1,27 @@
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, CCLoader, Sprite, RenderTexture, SpriteFrame, Widget } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Index')
 export class Index extends Component {
 
+    @property(Sprite)
+    photo: Sprite = null;
+
+    @property(RenderTexture)
+    rt: RenderTexture = null;
+
+    @property(Widget)
+    widget: Widget = null;
+
     start () {
-        
+        var spf = new SpriteFrame();
+        spf.texture = this.rt;
+        this.photo.spriteFrame = spf;
+
+        this.widget.top = 0;
+        this.widget.right = 0;
+        this.widget.bottom = 0;
+        this.widget.left = 0;
     }
 }
